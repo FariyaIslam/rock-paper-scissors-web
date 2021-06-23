5460f7615bf99b8d1e10933032473258a7a1e776
Fariya Islam <fariyaislam2096@gmail.com>
2021-06-23 21:45:18 +0600

    game completed & added sounds effect!

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
+const round = document.querySelector("#round");
+const counter = document.querySelector("#counter");
+const counterAI = document.querySelector("#counterAI");
+const winnerCard = document.querySelector(".winner__card");
+const game__winner = document.querySelector("#game__winner");
 
 // Adding event listeners
 rock.addEventListener("click", handleRock);
@@ -9,11 +14,16 @@ paper.addEventListener("click", handlePaper);
 scissors.addEventListener("click", handleScissors);
 
 
+
 // declearing variables
 let userSelection = "";
 let AISelection = "";
 let userCounter = 0;
+let roundCounter = 2;
 let AICounter = 0;
+let beep = new Audio("./audio/beep.wav");
+let beepError = new Audio("./audio/beepError.mp3");
+let tie = new Audio("./audio/gameTie.wav");
 
 //Event Handler functions
 function handleRock() {
@@ -50,6 +60,9 @@ function gameRun(userSelection, AISelection) {
     // User win
     let winner = "";
 
+    round.textContent = `ROUND : ${roundCounter}`; //Important , template literals 
+
+
     if (userSelection == "rock" && AISelection == "scissors") {
         winner = "User";
         userCounter++;
@@ -78,9 +91,30 @@ function gameRun(userSelection, AISelection) {
     if (userSelection == AISelection) {
         winner = "none";
     }
+
+
+    counter.innerHTML = `WIN : ${userCounter}`;
+    counterAI.innerHTML = `WIN : ${AICounter}`;
+    roundCounter++;
+
+    const winnerElement = document.createElement('p');
+    // winner.innerHTML = 
+    if (winner == "User") {
+        game__winner.innerHTML = `You are the Winner!`;
+        beep.play();
+    } else if (winner == "AI") {
+        game__winner.innerHTML = `Oh! dear, You lost to an AI! shame!`;
+        beepError.play();
+    } else {
+        game__winner.innerHTML = `No one is better, It's a tie!! haha`;
+        tie.play();
+    }
+
     console.log("The winner is : ", winner);
     console.log("user Counter ", userCounter, ",Ai Counter: ", AICounter);
 }
+
+
 // gameRun()
 
 // rock + scissors = rock win
